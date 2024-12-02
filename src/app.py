e68ef1c23db5fdb39572102a56bf9ea468afb6a5
George Perdrizet <george@perdrizet.org>
2024-12-02 15:39:46 -0500

    Added solution outline.

--- src/app.py
+++ src/app.py
@@ -19,31 +19,20 @@ class_dict={
 # Page title
 st.title('Diabetes prediction')
 
-# Sliders for input data
-glucose=st.slider('Glucose', min_value=0.0, max_value=200.0, step=1.0)
-insulin=st.slider('Insulin', min_value=0.0, max_value=300.0, step=1.0)
-bmi=st.slider('BMI', min_value=0.0, max_value=100.0, step=1.0)
-age=st.slider('Age', min_value=1, max_value=90, step=1)
+# Set up inputs for the user to enter data - take a look at the tutorial
+# for a hint on how to do this. Also, if you are using the model
+# supplied above, you need to send it four features: Glucose,
+# Insulin, BMI and Age
 
 # When the user clicks 'Predict'
 if st.button('Predict'):
 
-    # Format the data for inference
-    data=pd.DataFrame.from_dict({
-        'Glucose': [glucose],
-        'Insulin': [insulin],
-        'BMI': [bmi],
-        'Age': [age]
-    })
+        # Format the data for input into the model. For the model
+        # Supplied above, it should be a Pandas dataframe with four
+        # columns, one for each feature and one row.
 
-    # Print out the input features to the terminal for troubleshooting
-    print(data.head())
-
-    # Do the prediction
-    prediction=str(model.predict(data)[0])
-
-    # Convert the predicted value to a human readable string
-    pred_class=class_dict[prediction]
+        # Then do the prediction and covert the class number that the
+        # model returns to a human readable string, like 'diabetic' etc.
 
     # Display the prediction to the user
-    st.write('Prediction:', pred_class)
+    st.write('Prediction:', predicted_class)
